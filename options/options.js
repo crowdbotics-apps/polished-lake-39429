4bda4b00938698699866f77eeabaffbe1144ceb8
Crowdbotics <team@crowdbotics.com>
2023-03-17 15:08:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-lake-39429",
+  url: "https://polished-lake-39429.botics.co",
+  api: "https://polished-lake-39429.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
